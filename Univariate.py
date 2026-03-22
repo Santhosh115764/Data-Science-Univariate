f750d3025e03f983444ee6132c2160735da4a2ea
Santhosh115764 <bai866430@gmail.com>
2026-03-22 21:05:36 +0530

    Add files via upload

--- Univariate.py
+++ Univariate.py
@@ -13,4 +13,29 @@ class Univariate():
                 quan.append(columnName)
         return quan,qual
     
+    def freqTable(columnName,dataset):
+        freqTable=pd.DataFrame(columns=["Unique_values","Frequency","Relative Frequency","Cusum"])
+        freqTable["Unique_values"]=dataset[columnName].value_counts().index
+        freqTable["Frequency"]=dataset[columnName].value_counts().values
+        freqTable["Relative Frequency"]=(freqTable["Frequency"]/103)
+        freqTable["Cusum"]=freqTable["Relative Frequency"].cumsum()
+        return freqTable
     
+    def Univariate(dataset,quan):
+        descriptive=pd.DataFrame(index=["mean","median","mode","Q1:25%",
+                                    "Q2:50%","Q3:75%","Q4:100%","IQR","1.5rule","Lesser","Greater","Min","Max"],columns=quan)
+        for columnName in quan:
+            descriptive[columnName]["mean"]=dataset[columnName].mean()
+            descriptive[columnName]["median"]=dataset[columnName].median()
+            descriptive[columnName]["mode"]=dataset[columnName].mode()[0]
+            descriptive[columnName]["Q1:25%"]=dataset.describe()[columnName]["25%"]
+            descriptive[columnName]["Q2:50%"]=dataset.describe()[columnName]["50%"]
+            descriptive[columnName]["Q3:75%"]=dataset.describe()[columnName]["75%"]
+            descriptive[columnName]["Q4:100%"]=dataset.describe()[columnName]["max"]
+            descriptive[columnName]["IQR"]=descriptive[columnName]["Q3:75%"]-descriptive[columnName]["Q1:25%"]
+            descriptive[columnName]["1.5rule"]=1.5*descriptive[columnName]["IQR"]
+            descriptive[columnName]["Lesser"]=descriptive[columnName]["Q1:25%"]-descriptive[columnName]["1.5rule"]
+            descriptive[columnName]["Greater"]=descriptive[columnName]["Q3:75%"]+descriptive[columnName]["1.5rule"]
+            descriptive[columnName]["Min"]=dataset[columnName].min()
+            descriptive[columnName]["Max"]=dataset[columnName].max()
+        return descriptive
